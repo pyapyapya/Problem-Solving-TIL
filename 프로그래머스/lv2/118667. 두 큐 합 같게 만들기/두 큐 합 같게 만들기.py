cf4929f8a45c8ea774e4c72ac0b677cc8feba3f3
TaeHyeon Jeon <42240862+pyapyapya@users.noreply.github.com>
2022-09-06 22:31:39 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 50.62 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,32 +1,26 @@
-from collections import deque
-
 def solution(queue1, queue2):
+    head = 0
+    tail = len(queue1)
     sum1 = sum(queue1)
     sum2 = sum(queue2)
-    length = len(queue1) * 2
+    queue1.extend(queue2)
+
     half_sum = (sum1 + sum2) // 2
     
     if sum1 == half_sum:
         return 0
-    
-    arr = queue1.extend(queue2)
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    idx1 = 0
-    idx2 = 0
     answer = 0
-    while idx1 < length and idx2 < length:
+    while tail < len(queue1):
         if sum1 == half_sum:
-            return idx1 + idx2
+            return answer
         elif sum1 > half_sum:
-            sum1 -= queue1[0]
-            sum2 += queue1[0]
-            idx1 += 1
-            queue2.append(queue1.popleft())
+            sum1 -= queue1[head]
+            sum2 += queue1[head]
+            head += 1
+            answer += 1
         else:
-            sum2 -= queue2[0]
-            sum1 += queue2[0]
-            idx2 += 1
-            queue1.append(queue2.popleft())
-        answer += 1
+            sum1 += queue1[tail]
+            sum2 -= queue1[tail]
+            tail += 1
+            answer += 1
     return -1
