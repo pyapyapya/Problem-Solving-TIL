eecb15c0c41ab5c1c9e425716d8259a4fa7eeecf
TaeHyeon Jeon <42240862+pyapyapya@users.noreply.github.com>
2025-01-03 21:06:13 +0900

    [level 1] Title: 추억 점수, Time: 1.90 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,6 +1,7 @@
 def solution(name, yearning, photos):
     answer = []
     yearn = dict(zip(name, yearning))
+
     for photo in photos:
         score = 0
         yearn_people = set(name) & set(photo)
